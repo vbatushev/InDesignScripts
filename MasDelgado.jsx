22c199bf2d68c8e99511af57043ff7ba5bf94c5e
Vitaly Batushev <vitaly@batushev.info>
2013-12-22 23:18:32 -0430

    Add scripts InBackup, MasDelgado, InCountChars, win2utf

---
+++ MasDelgado.jsx
@@ -0,0 +1,40 @@
+﻿/* =========================================================================
+
+NOMBRE: MasDelgado.jsx
+VERSÍON: 1.7
+
+ESCRITOR: Vitaly Batushev
+FECHA: 29.09.2012
+
+SOPORTE: http://szam5.com
+CORREO: vitaly@batushev.info
+
+============================================================================ */
+var strSelectFile = 'Выберите файл для улучшения';
+var strQuestionRepair = 'Улучшить активную публикацию?';
+var strQuestionOpen = 'Открыть улучшенную публикацию?';
+ if (app.documents.length == 0) {
+	var mySourceFile = File.openDialog(strSelectFile, '*.INDD');
+	if (mySourceFile == null) { exit(); }
+} else {
+	var rst = confirm(strQuestionRepair);
+	if (rst == true) { 
+		var mySourceFile = new File(app.activeDocument.fullName);
+		app.activeDocument.close(1852776480);
+	} else {
+		var mySourceFile = File.openDialog(strSelectFile, '*.INDD');
+		if (mySourceFile == null) { exit(); }
+	}
+}
+var tmpFolder = Folder(mySourceFile.path);
+var idmlFile = new File(tmpFolder + '/temp.idml');
+var myName = mySourceFile.fsName;
+var bakName = myName.substr(0, myName.length - 5) + '-bak.indd';
+var myDoc = app.open(mySourceFile, false);
+myDoc.exportFile(ExportFormat.INDESIGN_MARKUP, idmlFile);
+myDoc.close(1852776480);
+mySourceFile.rename(bakName);
+var newDoc = app.open(idmlFile, false);
+newDoc.close(2036691744, myName);
+idmlFile.remove();
+if (confirm(strQuestionOpen)) { app.open(myName, true); }
---
+++ win2utf.jsx
@@ -0,0 +1,18 @@
+﻿// Vitaly Batushev win2utf
+
+function d2h(d) {return d.toString(16);} function h2d(h) {return parseInt(h,16);}  
+with (app) {  
+    if (documents.length == 0) { exit(); }  
+    if (selection.length > 0) { 
+        for (s = 0; s < selection.length; s ++) { try { cyr2utf(selection[s]); } catch(e) {} } 
+    } else { 
+        cyr2utf(activeDocument); 
+    } 
+} 
+function cyr2utf(obj) { 
+    app.findGrepPreferences = null; app.changeGrepPreferences = null; 
+    app.findGrepPreferences.findWhat = '\\x{00B8}'; app.changeGrepPreferences.changeTo = 'ё'; obj.changeGrep();  
+    app.findGrepPreferences.findWhat = '\\x{00A8}'; app.changeGrepPreferences.changeTo = 'Ё'; obj.changeGrep();  
+    for (i = 192; i < 256; i++) { app.findGrepPreferences.findWhat = '\\x{00' + d2h(i).toUpperCase() + '}';  app.changeGrepPreferences.changeTo = '\\x{0' + d2h(i + 848).toUpperCase()  + '}'; obj.changeGrep(); } 
+    app.findGrepPreferences = null; app.changeGrepPreferences = null;  
+}  
