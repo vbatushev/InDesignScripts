4c5613e75bea982bfda435ebeba8a142d2990062
Vitaly Batushev <vbatushev@gmail.com>
2015-12-07 02:47:06 +0700

    Версия 2.0 Добавлена обработка сносок Добавлен прогрессбар Переписано всякое Убран русский язык

--- InCountChars.jsx
+++ InCountChars.jsx
@@ -1,184 +1,190 @@
-﻿/* =========================================================================
-
-InDesign ExtendScript
-
-NAME: InCountChars
-VERSION: 1.2
-
-AUTHOR: Vitaly Batushev
-DATE  : 22.12.2013
-
-DESCRIPTION: Статистика о количестве знаков в публикации
-
-E-MAIL: vitaly@batushev.info
-
-============================================================================ */
+﻿/*
+* @Author: vbatushev
+* @Date:   2015-09-01 09:25:06
+* @version:    2.0
+* @Last Modified by:   vbatushev
+* @Last Modified time: 2015-12-07 02:40:49
+*/
 
 // Список исключаемых символов по умолчанию.
 // Пишется в формате GREP. Можно использовать GREP-символы Adobe InDesign.
 // При перечислении символы разделяются символом |
 var excludeSymbols = "~-";
 
-
-with (app) {
-var ScriptName = getFileNameOnly(app.activeScript.name) + ' 1.2';
-try {		
-	if (app.version.substr(0,1) > 3 ) {
-		$.locale = "ru";
-		//$.localize = true;
-		var strMinWords = localize({en: 'Minimum words in Story', ru: 'Минимальное количество слов в статье:'});
-		var strViewWithOutSpaces = localize({en: 'Show numbers characters without spaces', ru: 'Показывать количество знаков без пробелов'});
-		var strViewWithOutSpecial = localize({en: 'Show numbers characters without special symbols', ru: 'Показывать количество знаков без указанных символов:'});
-		var strAttention = localize({en: 'Attention!', ru: 'Внимание!'});
-		var strNoOpenDocument = localize({en: 'No open documents!', ru: 'Нет открытых документов!'});
-		var strReport = localize({en: 'Report', ru: 'Статистический отчет'});
-		var strPublication = localize({en: 'Publication', ru: 'Название публикации'});
-		var strCharsWithSpaces = localize({en: 'characters with spaces', ru: 'символов, включая пробелы'});
-		var strCharsWithoutSpaces = localize({en: 'characters without spaces', ru: 'символов, не включая пробелы'});
-		var strCharsWithoutSymbols = localize({en: 'characters exclude', ru: 'символов, не включая'});
-		var strAuthorLists = localize({en: 'Authors lists', ru: 'Авторских листов'});
-		var strLessOneList = localize({en: 'less than one authors list', ru: 'менее одного авторского листа'});
-		var strSaveReport = localize({en: 'Save report in file?', ru: 'Сохранить отчет в файл?'});
-		var strTextFiles = localize({en: 'Text Files', ru: 'Текстовые файлы'});
-		var strSelectResultFile = localize({en: 'Save Result File', ru: 'Сохранить результат'});
-		var strEndWork = localize({en: 'Report created!', ru: 'Создание отчета завершено!'});
-		var strNoSaveFile = localize({en: 'File is not saved', ru: 'Файл не сохранен!'});
-		var strFileSaveAs = localize({en: 'File save as', ru: 'Файл сохранен как'});
-		var strCreatedWith = localize({en: 'Created with script', ru: 'Создано с помощью скрипта'});
-		var strCredits = localize({en: 'by Vitaly Batushev (https://github.com/vbatushev/)', ru: 'Виталий Батушев (https://github.com/vbatushev/)'});
-		var strBars = localize({en: '===============================', ru: '==============================='});
-	} else {
-		var strMinWords = 'Minimum words in Story';
-		var strViewWithOutSpaces = 'Show numbers characters without spaces';
-		var strViewWithOutSpecial = 'Show numbers characters without special symbols';
-		var strAttention = 'Attention!';
-		var strNoOpenDocument = 'No open documents!';
-		var strReport = 'Report';
-		var strPublication = 'Publication';
-		var strCharsWithSpaces = 'characters with spaces';
-		var strCharsWithoutSpaces = 'characters without spaces';
-		var strCharsWithoutSymbols = 'characters exclude';
-		var strAuthorLists = 'Authors lists';
-		var strLessOneList = 'less than one authors list';
-		var strSaveReport = 'Save report in file?';
-		var strTextFiles = 'Text Files';
-		var strSelectResultFile = 'Save Report File';
-		var strEndWork = 'Report created!';
-		var strNoSaveFile = 'File is not saved';
-		var strFileSaveAs = 'File save as';
-		var strCreatedWith = 'Created with script';
-		var strCredits = 'by Vitaly Batushev (http://indesing.rudtp.ru)';
-		var strBars = '===============================';
-	}
-} catch (e) {
-	var strMinWords = 'Minimum words in Story';
-	var strViewWithOutSpaces = 'Show numbers characters without spaces';
-	var strViewWithOutSpecial = 'Show numbers characters without special symbols';
-	var strCharsWithoutSymbols = 'characters exclude';
-	var strAttention = 'Attention!';
-	var strNoOpenDocument = 'No open documents!';
-	var strReport = 'Report';
-	var strPublication = 'Publication';
-	var strCharsWithSpaces = 'characters with spaces';
-	var strCharsWithoutSpaces = 'characters without spaces';
-	var strAuthorLists = 'Authors lists';
-	var strLessOneList = 'less than one authors list';
-	var strSaveReport = 'Save report in file?';
-	var strTextFiles = 'Text Files';
-	var strSelectResultFile = 'Save Report File';
-	var strEndWork = 'Report created!';
-	var strNoSaveFile = 'File is not saved';
-	var strFileSaveAs = 'File save as';
-	var strCreatedWith = 'Created with script';
-	var strCredits = 'by Vitaly Batushev (http://szam5.com)';
-	var strBars = '===============================';
-}
-	if (documents.length < 1) { alert(strNoOpenDocument, strAttention); exit(); }
-	var myDoc = activeDocument;	
-	var myDlg = dialogs.add({name:ScriptName});
-	with(myDlg) {
-		with(dialogColumns.add()) {
-			with (dialogRows.add()) {
-				staticTexts.add({staticLabel:strMinWords});
-				var minWords = integerEditboxes.add({editValue:0, minWidth:25});
-			}
-			with (dialogRows.add()) {
-				staticTexts.add({staticLabel:strViewWithOutSpecial});
-			}
-			with (dialogRows.add()) {
-				var outSmbl = textEditboxes.add({editContents:excludeSymbols, minWidth:340});
-			}
-			var chkWithOutSpace = checkboxControls.add({staticLabel:strViewWithOutSpaces, checkedState:false})
-		}
-	}
-	result = myDlg.show();
-	if (result == false) { myDlg.destroy(); exit();	}
-	var fullCount = 0;
-	var spaceCount = 0;
-	var outSymbols = 0;
-	for (i = 0; i < myDoc.stories.length; i++) {
-		var myStory = myDoc.stories[i];
-		if (myStory.words.length > minWords.editValue) {
-			fullCount = fullCount + myStory.characters.length;
-			if (chkWithOutSpace.checkedState == true ){
-				var  re = /\s/g;
-				try {
-					var tmpSpace = myStory.contents.match(re);
-				} catch (e) {}
-				if (tmpSpace != null) { spaceCount = spaceCount + tmpSpace.length; }
-			}
-			if (outSmbl.editContents != "") {
-				findGrepPreferences = null;
-				findGrepPreferences = null;
-				findGrepPreferences.findWhat = outSmbl.editContents;
-				var findSymbols = myStory.findGrep();
-				outSymbols += findSymbols.length;				
-				changeGrepPreferences = null;
-			}
-			if (myStory.tables.length > 0) {
-				for (ti = 0; ti < myStory.tables.length; ti++) {
-					var myTable = myStory.tables[ti];
-					for (j = 0; j < myTable.cells.length; j++) {
-						fullCount = fullCount + myTable.cells[j].characters.length;
-						if (chkWithOutSpace.checkedState == true ){ 
-							try {
-								var tmpSpace = myTable.cells[j].contents.match(re);
-							} catch (e) {}
-							if (tmpSpace != null) { spaceCount = spaceCount + tmpSpace.length; }
-						}
-					}
-				}
-			}
-		}
-	}
-	var aList = Math.round(fullCount/40000);
-	if (aList < 1) { aList = strLessOneList };
-	var txtReport = strBars + '\n' + strReport + '\n' + strBars + '\n\n' + 
-	strPublication + ': ' + activeDocument.name + '\n\n' +
-	fullCount + ' ' + strCharsWithSpaces + '.' + '\n';
-	if (chkWithOutSpace.checkedState == true ){
-		txtReport += (fullCount - spaceCount) + ' ' + strCharsWithoutSpaces + '.' + '\n\n';
-	}
-	if (outSymbols > 0) {
-		txtReport += (fullCount - outSymbols) + ' ' + strCharsWithoutSymbols + " " + outSmbl.editContents + '.' + '\n\n';
-	}
-	txtReport = txtReport + strAuthorLists + ': ' + aList + '.' + '\n\n\n\n' + strBars + '\n' + 
-	strCreatedWith + ' ' + ScriptName + '\n' + strCredits;
-	alert (txtReport, ScriptName);
-
-	var myResultFile = new File (activeDocument.filePath + "/Report.txt");
-	myResultFile.encoding = 'UTF16';	
-	myResultFile.open("w");		
-	myResultFile.write(txtReport);
-	myResultFile.close();	
-	myResultFile.execute();
-	
+app.scriptPreferences.enableRedraw = true;
+var strings = {
+    name: 'inCountChars 2.0',
+    minWords: 'Minimum words in Story',
+    viewWithOutSpaces: 'Show numbers characters without spaces',
+    viewWithOutSpecial: 'Show numbers characters without special symbols',
+    attention: 'Attention!',
+    noOpenDocument: 'No open documents!',
+    report: 'Report',
+    publication: 'Publication',
+    charsWithSpaces: 'characters with spaces',
+    charsWithoutSpaces: 'characters without spaces',
+    charsWithoutSymbols: 'characters exclude',
+    authorLists: 'Authors lists',
+    lessOneList: 'less than one authors list',
+    saveReport: 'Save report in file?',
+    textFiles: 'Text Files',
+    selectResultFile: 'Save Report File',
+    endWork: 'Report created!',
+    noSaveFile: 'File is not saved',
+    fileSaveAs: 'File save as',
+    createdWith: 'Created with script',
+    credits: 'by Vitaly Batushev (https://github.com/vbatushev/InDesignScripts)',
+    bars: '===============================',
 }
 
-function getFileNameOnly (myFileName) {
-	var myString = "";
-	var myResult = myFileName.lastIndexOf(".");
-	if (myResult == -1) { myString = myFileName; }
-	else { myString = myFileName.substr(0, myResult); }
-	return myString;
+if (app.documents.length < 1) { alert(strings.noOpenDocument, strings.attention); exit(); }
+
+var dlg = app.dialogs.add({name:strings.name});
+with(dlg) {
+    with(dialogColumns.add()) {
+        with (dialogRows.add()) {
+            staticTexts.add({staticLabel:strings.minWords});
+            var minWords = integerEditboxes.add({editValue:0, minWidth:25});
+        }
+        with (dialogRows.add()) {
+            staticTexts.add({staticLabel:strings.viewWithOutSpecial});
+        }
+        with (dialogRows.add()) {
+            var outSmbl = textEditboxes.add({editContents:excludeSymbols, minWidth:340});
+        }
+        var chkWithOutSpace = checkboxControls.add({staticLabel:strings.viewWithOutSpaces, checkedState:false})
+    }
+}
+result = dlg.show();
+if (result == false) { dlg.destroy(); exit(); }
+var counter = new getCountChars();
+counter.exclude = outSmbl.editContents;
+counter.spaces = chkWithOutSpace.checkedState;
+counter.min = minWords.editValue;
+counter.run();
+
+function getCountChars() {
+    this.exclude = excludeSymbols;
+    this.min = 0;
+    this.spaces = false;
+    this.progress;
+
+    this.run = function() {
+        this.progress = new ProgressbarClass(app.activeDocument.stories.length, strings.name);
+        var chars = {spaces: 0, symbols: 0, total: 0 }
+        for (i = 0; i < app.activeDocument.stories.length; i++) {
+            var myStory = app.activeDocument.stories[i];
+            if (myStory.words.length > this.min) {
+                var symbols = this.getChars(myStory, false);
+                chars.total += symbols.total;
+                chars.spaces += symbols.spaces;
+                chars.symbols += symbols.symbols;
+
+                for (ti = 0; ti < myStory.tables.length; ti++) {
+                    var myTable = myStory.tables[ti];
+                    for (j = 0; j < myTable.cells.length; j++) {
+                        var tblSymbols = this.getChars(myTable.cells[j],false);
+                        chars.total += tblSymbols.total;
+                        chars.spaces += tblSymbols.spaces;
+                        chars.symbols += tblSymbols.symbols;
+                    }
+                }
+
+                for (f = 0; f < myStory.footnotes.length; f++) {
+                    var fSymbols = this.getChars(myStory.footnotes[f],true);
+                    chars.total += fSymbols.total;
+                    chars.spaces += fSymbols.spaces;
+                    chars.symbols += fSymbols.symbols;
+                }
+            }
+            this.progress.increase();
+        }
+        this.save(chars);
+        this.progress.close();
+    }
+
+    this.save = function (result) {
+        var aList = Math.round(result.total/40000);
+        if (aList < 1) { aList = strings.lessOneList };
+        var txtReport = strings.bars + '\n' + strings.report + '\n' + strings.bars + '\n\n' +
+        strings.publication + ': ' + app.activeDocument.name + '\n\n' +
+        result.total + ' ' + strings.charsWithSpaces + '.' + '\n';
+        if (this.spaces){
+            txtReport += (result.total - result.spaces) + ' ' + strings.charsWithoutSpaces + '.' + '\n\n';
+        }
+        if (result.symbols > 0) {
+            txtReport += (result.total - result.symbols) + ' ' + strings.charsWithoutSymbols + " " + this.exclude + '.' + '\n\n';
+        }
+
+        txtReport = txtReport + strings.authorLists + ': ' + aList + '.' + '\n\n\n\n' + strings.bars + '\n' +
+        strings.createdWith + ' ' + strings.name + '\n' + strings.credits;
+        alert (txtReport, strings.name);
+
+        var myResultFile = new File (app.activeDocument.filePath + "/Otchet.txt");
+        myResultFile.encoding = 'UTF8';
+        myResultFile.open("w");
+        myResultFile.write(txtReport);
+        myResultFile.close();
+        myResultFile.execute();
+    }
+
+    this.getChars = function(obj, footnote) {
+        var out = new Object();
+        var content = obj.contents;
+        out.total = obj.characters.length;
+        var  re = /\s/g;
+        var tmpSpace = content.match(re);
+        if (tmpSpace === null) { out.spaces = 0; } else { out.spaces = tmpSpace.length; }
+        resetPrefs();
+        app.findGrepPreferences.findWhat = this.exclude;
+        var findSymbols;
+        if (footnote) {
+            for (a = 0; a < obj.paragraphs.length; a++) {
+                app.findChangeObjectOptions.includeFootnotes = false;
+                findSymbols = obj.paragraphs[a].findGrep();
+                out.symbols = findSymbols.length;
+            }
+        } else {
+            app.findChangeObjectOptions.includeFootnotes = true;
+            findSymbols = obj.findGrep();
+            out.symbols = findSymbols.length;
+        }
+        resetPrefs();
+        return out;
+    }
+
+    function resetPrefs() {
+        app.findGrepPreferences = null;
+        app.findGrepPreferences = null;
+        app.changeGrepPreferences = null;
+    }
+
+    function ProgressbarClass (maxVal, barLabel) {
+        var win = new Window("palette", barLabel, [150, 150, 600, 260]);
+        this.windowRef = win;
+        win.pnl = win.add("panel", [10, 10, 440, 100], "");
+
+        win.pnl.progBarLabel = win.pnl.add("statictext", [20, 15, 380, 30], "Calculating...");
+        win.pnl.progBar = win.pnl.add("progressbar", [20, 35, 405, 60], 0, maxVal);
+
+        win.stopButton = win.add("button", [150, 110, 250, 140], "Stop");
+        win.stopButton.onClick = function ()  { this.terminated = true; };
+        this.terminated = false;
+
+        win.onClose = function() {}
+
+        win.center();
+        win.show();
+
+        this.close = function() { win.close(); };
+        this.reset = function (newMaxVal, newBarLabel) {
+            this.windowRef.pnl.progBar.maxvalue = newMaxVal;
+            this.windowRef.pnl.progBar.value = 0;
+            this.windowRef.pnl.progBarLabel.text = newBarLabel;
+        };
+        this.setVal = function (progBarVal) { this.windowRef.pnl.progBar.value = progBarVal; };
+        this.increase = function () { this.windowRef.pnl.progBar.value++; };
+        this.setLabel = function (St) { this.windowRef.pnl.progBarLabel.text = St; };
+        this.setPanelLabel = function (St) { this.windowRef.pnl.text = St; };
+    }
 }
